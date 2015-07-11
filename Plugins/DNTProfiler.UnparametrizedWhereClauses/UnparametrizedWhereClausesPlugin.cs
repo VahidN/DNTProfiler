39ba4f6afd11f0e9c86b8fd17f5414ab3b7be988
VahidN <vahid_nasiri@yahoo.com>
2015-07-11 11:16:08 +0430

    Improved comments of UnparametrizedWhereClausesPlugin

--- Plugins/DNTProfiler.UnparametrizedWhereClauses/UnparametrizedWhereClausesPlugin.cs
+++ Plugins/DNTProfiler.UnparametrizedWhereClauses/UnparametrizedWhereClausesPlugin.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using DNTProfiler.PluginsBase;
 
 namespace DNTProfiler.UnparametrizedWhereClauses
@@ -10,7 +11,19 @@ namespace DNTProfiler.UnparametrizedWhereClauses
             PluginMetadata = new PluginMetadata
             {
                 PluginName = "Unparameterized Where Clauses",
-                PluginDescription = "Unparameterized queries(ad-hoc queries) will cause more resource consumption (higher CPU and Memory usage of the the database server). Also if you are calling these queries using the `Database.SqlQuery/ExecuteSqlCommand` method directly, be aware of possible SQL injection attacks."
+                PluginDescription =
+                "Unparameterized queries(ad-hoc queries) will cause more resource consumption (higher CPU and Memory usage of the the database server)."+
+                Environment.NewLine +
+                " Also if you are calling these queries using the `Database.SqlQuery/ExecuteSqlCommand` method directly, be aware of possible SQL injection attacks." +
+                Environment.NewLine +
+                @"If you are using SQL Server, try enabling parameterization:
+-- Enabling Parameterization
+ALTER DATABASE dbName SET PARAMETERIZATION FORCED;
+-- Optimizing for Ad hoc Workloads
+sp_configure 'show advanced options',1;
+RECONFIGURE;
+sp_configure 'optimize for ad hoc workloads',1;
+RECONFIGURE;" + Environment.NewLine
             };
 
             PluginAuthor = new PluginAuthor
